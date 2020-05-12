3a1a4e57c6c096ff3a160a8c1c18bdf00a90051a
kaito.kitagawa <kaito.naniwa.habu@gmail.com>
2020-05-12 09:00:40 +0000

    for heroku

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,6 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
+  before_action :correct_user, only: [:destroy]
   def index
     @tasks = Task.all
   end
@@ -13,12 +14,13 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
 
     if @task.save
       flash[:success] = 'タスクが正常に投稿されました'
       redirect_to @task
     else
+      @tasks = current_user.tasks.order(id: :desc).page(params[:page])
       flash.now[:danger] = 'タスクが投稿されませんでした'
       render :new
     end
@@ -41,11 +43,11 @@ class TasksController < ApplicationController
   end
 
   def destroy
-    @task = Task.find(params[:id])
     @task.destroy
+    flash[:success] = 'メッセージを削除しました。'
+    redirect_to root_url
 
-    flash[:success] = 'タスクは正常に削除されました'
-    redirect_to tasks_url
+    
   end
   
   
@@ -53,6 +55,13 @@ class TasksController < ApplicationController
 
   def task_params
     params.require(:task).permit(:content, :status)
+  end
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
   end
 end
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
- 
+  before_action :require_user_logged_in, only: [:create]
+  before_action :correct_user, only: [:destroy]
   def index
   end
 
