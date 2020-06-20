e74aedb4a4a83a518a39468a8b54b47a1e8f8a72
kaito.kitagawa <kaito.naniwa.habu@gmail.com>
2020-06-20 12:42:22 +0000

    kadai-modify3

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,7 +2,7 @@ class TasksController < ApplicationController
   before_action :require_user_logged_in
   before_action :correct_user, only: [:destroy]
   def index
-    @tasks = current_user.tasks.order(id: :desc)
+      @tasks = current_user.tasks.order(id: :desc)
   end
 
   def show
@@ -18,7 +18,7 @@ class TasksController < ApplicationController
 
     if @task.save
       flash[:success] = 'タスクが正常に投稿されました'
-      redirect_to @task
+      redirect_to root_url
     else
       @tasks = current_user.tasks.order(id: :desc).page(params[:page])
       flash.now[:danger] = 'タスクが投稿されませんでした'
@@ -57,7 +57,12 @@ class TasksController < ApplicationController
     params.require(:task).permit(:content, :status)
   end
   
-  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
+  end
 end
 
 
